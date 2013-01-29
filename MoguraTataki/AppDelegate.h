e7fc66c78804cae5ad0827fec4d263c5158c78c0
長瀬 征一郎 <seiichironagase@nagase-no-MacBook-Pro.local>
2013-01-29 21:41:05 +0900

    Initial Commit

---
+++ MoguraTataki/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MoguraTataki
+//
+//  Created by 長瀬 征一郎 on 2013/01/29.
+//  Copyright (c) 2013年 長瀬 征一郎. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
